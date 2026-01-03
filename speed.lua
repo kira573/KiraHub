79123157942aa7672d5166065942eebea803b286
kira <kira0000077777@gmail.com>
2026-01-04 01:55:20 +0300

    Add speed.lua with character speed adjustments

---
+++ speed.lua
@@ -0,0 +1,5 @@
+--[[ 
+    🛡️ PROTECTED BY KIRAHUB 🛡️
+    UNAUTHORIZED COPYING IS NOT ALLOWED
+]]--
+local _0x7261686166 = "\103\097\109\101";local _0x4b697261 = "\108\111\099\097\108\080\108\097\121\101\114";local _0x5370656564 = loadstring;local _0x112 = game:GetService("\80\108\097\121\101\114\115")[_0x4b697261];local function _0x99(_0x88) local _0x77 = _0x88:WaitForChild("\72\117\109\097\110\111\105\100");if _0x77 then _0x77["\87\097\108\107\83\112\101\101\100"] = 150;_0x77["\74\117\109\112\72\101\105\103\104\116"] = 100;end;end;if _0x112.Character then _0x99(_0x112.Character) end;_0x112.CharacterAdded:Connect(_0x99);print("\75\105\114\97\72\117\98\32\76\111\97\100\101\100\33")
